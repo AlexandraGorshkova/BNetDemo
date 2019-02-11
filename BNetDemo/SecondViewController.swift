a96ba322d0bc1b9f0a5d3241e341b33b5314be74
AlexandraGorshkova <41120236+AlexandraGorshkova@users.noreply.github.com>
2019-02-11 22:25:17 +0300

    Update SecondViewController.swift

--- BNetDemo/SecondViewController.swift
+++ BNetDemo/SecondViewController.swift
@@ -10,16 +10,12 @@ import UIKit
 
 class SecondViewController: UIViewController {
     @IBOutlet weak var da: UILabel!
-    @IBOutlet weak var dm: UILabel!
-  //  @IBOutlet weak var textData: UITextView!
-    
+    @IBOutlet weak var dm: UILabel!    
     @IBOutlet weak var textDd: UITextView!
     var nameDa: String = ""
     var nameDM: String = ""
     var textD: String = ""
     
-    
-    //@IBOutlet weak var textData: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,19 +23,5 @@ class SecondViewController: UIViewController {
         da.text = nameDa
         dm.text = nameDM
         textDd.text = textD
-        
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
